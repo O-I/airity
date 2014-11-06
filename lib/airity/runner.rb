e455e5ae5be7777565c6e1e27ca10dba1fef5f7a
Rahul Horé <hore.rahul@gmail.com>
2014-11-05 19:04:06 -0500

    Refactors and modularizes

--- lib/airity/runner.rb
+++ lib/airity/runner.rb
@@ -10,14 +10,83 @@ module Airity
       # Use ~/.airity configuration file
       configure
 
-      # Collect credentials from command line
-      email = ask('Email:') { |q| q.echo = true }
-      password = ask('Password:') { |q| q.echo = false }
-
       # Navigate to Google+ log in page
       visit '/'
 
       # Log in, if needed
+      log_in
+
+      # Allow for 2FA, if required
+      two_factor_authenticate
+
+      # Navigate to Google+ Hangouts on Air page
+      visit '/hangouts/onair'
+
+      start_hangout
+
+      # Clear Audience text field
+      clear_audience_text_field
+
+      # Invite this month's speakers
+      send_invitations
+
+      # Remove Public from Audience...again
+      ensure_private_hangout
+
+      # Share the Hangout privately
+      share_hangout
+
+      # Email the public YouTube link — not working yet
+      # find('div', text: 'Links').click
+
+      # Make it full screen
+      make_it_full_screen
+
+      # Wait for some input so script doesn't exit
+      puts 'Press `return` to quit'
+      STDIN.gets
+    end
+
+    private
+
+    def configure
+      begin
+        # this doesn't work yet!
+        # how do I require an absolute path?
+        require "Users/#{ENV['USER']}/.airity"
+      rescue LoadError
+        # File doesn't exist
+        @config ||= {}
+        puts "You can add a Users/#{ENV['USER']}/.airity file " \
+             'to preset commonly filled fields. '
+        puts 'Refer to the .airity_example file or the documentation ' \
+             'for more information.'
+      end
+    end
+
+    def email
+      @config[:email] || ask('Email: ') { |q| q.echo = true }
+    end
+
+    def password
+      @config[:password] || ask('Password: ') { |q| q.echo = false }
+    end
+
+    def authentication_token
+      ask('Token: ') { |q| q.echo = false }
+    end
+
+    def meeting_name
+      @config[:meeting_name] || ask('Meeting name: ') { |q| q.echo = true }
+    end
+
+    def invitees
+      puts 'Enter the email addresses of this month\'s speakers ' \
+           'separated by spaces (don\'t forget to invite yourself!)'
+      ask('Invitees:') { |q| q.echo = true }
+    end
+
+    def log_in
       begin
         find_field('Email')
         fill_in 'Email', with: email
@@ -26,55 +95,48 @@ module Airity
       rescue => e
         # User is already logged in
       end
+    end
 
-      # Allow for 2FA, if required
+    def two_factor_authenticate
       begin
-        authentication_token = ask('Token:') { |q| q.echo = false }
         fill_in 'Enter code', with: authentication_token
         click_button 'Verify'
       rescue => e
         # 2FA is not required
       end
+    end
 
-      # Navigate to Google+ Hangouts on Air page
-      visit '/hangouts/onair'
-
+    def start_hangout
       find('div[role="button"]', text: 'Start a Hangout On Air').click
-
-      meeting_name = #@config[:meeting_name] ||
-                     ask('Meeting name:') { |q| q.echo = true }
       fill_in 'Give it a name', with: meeting_name
+    end
 
-      # Clear Audience text field
+    def clear_audience_text_field
       find('div[title="Browse people"]').click
       find('span', text: 'selected', exact: false).click
       find('div[role="button"]', text: 'Unselect all').click
+    end
 
-      # prompt for email addresses of speakers and admin(s), e.g.,
-      puts 'Enter the email addresses of this month\'s speakers ' \
-           'separated by spaces (don\'t forget to invite yourself!)'
-      invitees = ask('Invitees:') { |q| q.echo = true }
-
+    def send_invitations
       find('input[aria-label="Search terms"]').set(invitees)
-
       find('div[role="button"]', text: 'Select all').click
       find('div[role="button"]', text: 'Done').click
+    end
 
-      # Remove Public from Audience...again
+    def ensure_private_hangout
       begin
         find('div[aria-label="Remove Public"]').click
       rescue => e
         # Public wasn't re-added
       end
+    end
 
+    def share_hangout
       find('div[role="button"]', text: 'Share').click
+    end
 
-      # Make it full screen
+    def make_it_full_screen
       `osascript -e 'tell application "System Events" to keystroke "f" using {command down, control down}'`
-
-      # Wait for some input so script doesn't exit
-      puts 'Press `return` to quit'
-      STDIN.gets
     end
   end
 end
